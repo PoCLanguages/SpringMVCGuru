0a8f29301f24382f6c472cf4ae261fd7154f3acc
kintat <tankintat@gmail.com>
2020-03-24 13:05:52 +0100

    act

---
+++ src/main/java/com/gft/springmvcguru/springmvc/services/ProductService.java
@@ -0,0 +1,16 @@
+package com.gft.springmvcguru.springmvc.services;
+
+import com.gft.springmvcguru.springmvc.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> listAllProducts();
+
+    Product getProductById(Integer id);
+
+    Product saveOrUpdateProduct(Product product);
+
+    void deleteProduct(Integer id);
+}
